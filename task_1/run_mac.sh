bc0622e22063c845a9b3866d2b4df401d6deed4c
mirosh111 <43246213+mirosh111@users.noreply.github.com>
2018-09-27 18:10:47 +0300

    s

--- task_1/run_mac.sh
+++ task_1/run_mac.sh
@@ -1,3 +1,3 @@
-conda env create -f environment_MacOS.yml
-source activate ml-tasks
+conda env create -f environment_mac.yml
+source activate mac_env
 jupyter-nbconvert --execute plot_iris_dataset.ipynb
